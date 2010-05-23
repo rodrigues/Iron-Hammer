78bff72ae4372fd8dbaa9c895ef70e276aff2beb
MACSkeptic (Mozair AC) <macskeptic@Gmail.com>
2010-05-23 14:43:37 -0300

    blablabla

--- lib/steel_hammer/helpers.rb
+++ lib/steel_hammer/helpers.rb
@@ -7,12 +7,13 @@ module SteelHammer::Helpers
 
     Dir["#{@@current_path}/helpers/*.rb"].each do |f|
       class_name = f.steel_hammer_class_name
-      load_and_include = %[
-        autoload :#{f.steel_hammer_class_name}, '#{f}'
-        include #{f.steel_hammer_class_name}
-      ]
-      eval(load_and_include) unless class_name == 'StringAddons'
-      base.instance_eval(load_and_include) unless class_name == 'StringAddons'
+
+      eval "autoload :#{f.steel_hammer_class_name}, '#{f}'"
+
+      inclusion = "include #{f.steel_hammer_class_name}"
+
+      eval(inclusion) unless class_name == 'StringAddons'
+      base.instance_eval(inclusion) unless class_name == 'StringAddons'
     end
   end
 end
