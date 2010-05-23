2a96343dd86960a828d3756c506aae2651505b94
MACSkeptic (Mozair AC) <macskeptic@Gmail.com>
2010-05-22 23:27:51 -0300

    major refactoring, tests passing

--- lib/steel_hammer/helpers.rb
+++ lib/steel_hammer/helpers.rb
@@ -2,7 +2,12 @@ module SteelHammer::Helpers
   @@current_path = File.dirname(__FILE__)
 
   def self.included(base)
-    require "#{@@current_path}/helpers/string_addons"
-    include StringAddons
+    #require "#{@@current_path}/helpers/string_addons"
+    #include StringAddons
+
+    Dir["#{@@current_path}/helpers*.rb"].each do |f|
+      eval "autoload :#{f.steel_hammer_class_name}, '#{f}'"
+      eval "include #{f.steel_hammer_class_name}"
+    end
   end
 end
