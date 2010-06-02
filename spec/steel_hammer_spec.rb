dd697c500241cc2fb57a5aa303f49c0de5d13c75
MACSkeptic (Mozair AC) <macskeptic@Gmail.com>
2010-06-01 22:10:01 -0300

    removed useless spec

--- spec/steel_hammer_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-
-describe "SteelHammer" do
-  it "passes" do
-    #fail "hey buddy, you should probably rename this file and start specing for real"
-    true.should be_true
-  end
-end
