8fdc17ea8147174859e0542935572b7eba1d95c3
Leandro Silva <leandrodoze@gmail.com>
2010-11-08 20:37:54 -0200

    added support to read IVY_ORGANISATION env variable

--- lib/iron_hammer/utils/ivy_configuration.rb
+++ lib/iron_hammer/utils/ivy_configuration.rb
@@ -17,7 +17,7 @@ module IronHammer
 
       private
       def initialize
-        @organisation = defined?(ORGANISATION) ? ORGANISATION : 'org'
+        @organisation = defined?(ORGANISATION) ? ORGANISATION : (ENV['IVY_ORGANISATION'] ? ENV['IVY_ORGANISATION'] : 'org')
         @ivy_jar = defined?(IVY_JAR) ? IVY_JAR : (ENV['IVY_JAR'] ? ENV['IVY_JAR'] : 'ivy.jar')
         @ivy_settings = defined?(IVY_SETTINGS) ? IVY_SETTINGS : (ENV['IVY_SETTINGS'] ? ENV['IVY_SETTINGS'] : 'ivysettings.xml')
         @retrieve_version = ENV['retrieve_version'] || 'latest.build'
